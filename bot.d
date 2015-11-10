985e1bdbaac13d9f772a6e3c093eeee04f2b629c
Zachary Snyder <sadin@fedoraproject.org>
2015-11-10 13:34:33 -0500

    Greeting + server connection details

--- bot.d
+++ bot.d
@@ -1,6 +1,28 @@
 import std.stdio;
 import std.socket;
 
-void main() {
-    writeln("Hello world!");
+class Server
+{
+  public:
+    int port;
+    string address;
+    string channel;
+
+}
+
+void main()
+{
+
+    writeln(" ____            _          ____        _   ");
+    writeln("|  _ \\ _ __ __ _| | _____  | __ )  ___ | |_ ");
+    writeln("| | | | '__/ _` | |/ / _ \\ |  _ \\ / _ \\| __|");
+    writeln("| |_| | | | (_| |   <  __/ | |_) | (_) | |_ ");
+    writeln("|____/|_|  \\__,_|_|\\_\\___| |____/ \\___/ \\__|");
+    writeln();
+
+    Server Server1 = new Server();
+
+    // Print out default connection details
+
+    //writeln("Connecting too ", address, " at port ", port);
 }
