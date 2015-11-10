8516737affcae4885e630fddccb260ef7b35a751
Zachary Snyder <sadin@fedoraproject.org>
2015-11-10 16:01:50 -0500

    edit

--- bot.d
+++ bot.d
@@ -1,20 +1,59 @@
-import std.stdio;
-import std.socket;
-import std.datetime;
+import std.stdio, std.socket, std.socketstream, std.conv, std.string, std.datetime;
 
-class Connection
+class IRCSocket
 {
-  public:
-    int port;
-    string address;
-    string channel;
-    string nick;
+private:
 
+    char[]         host;
+    ushort         port;
+    TcpSocket      sock;
+    SocketStream   stream;
+
+public:
+
+    this(char[] host, ushort port = 6667)
+    {
+        this.host = host;
+        this.port = port;
+        this.sock = null;
+    }
+
+    bool connected() {
+        return this.sock !is null;
+    }
+
+    bool connect()
+    {
+        this.sock   = new TcpSocket(new InternetAddress(this.host, this.port));
+        this.stream = new SocketStream(this.sock);
+        return true;
+    }
+
+    bool disconnect()
+    {
+        if (connected()) {
+            this.sock.close();
+            this.sock = null;
+            return true;
+        }
+        return false;
+    }
+
+    void close()
+    {
+        disconnect();
+    }
+
+    bool reconnect()
+    {
+        disconnect();
+        return connect();
+    }
 }
+
 
 void main()
 {
-
     writeln(" ____            _          ____        _   ");
     writeln("|  _ \\ _ __ __ _| | _____  | __ )  ___ | |_ ");
     writeln("| | | | '__/ _` | |/ / _ \\ |  _ \\ / _ \\| __|");
@@ -22,17 +61,16 @@ void main()
     writeln("|____/|_|  \\__,_|_|\\_\\___| |____/ \\___/ \\__|");
     writeln();
 
-    Connection freenode = new Connection();
+    Connection Connection1 = new Connection();
 
     // Declare default server connection information
-
-    freenode.port = 6667;
-    freenode.address = "irc.freenode.net";
-    freenode.channel = "##Codice";
-    freenode.nick = "Codice-Drakebot";
+    Connection1.address = "irc.freenode.net";
+    Connection1.channel = "##Codice";
+    Connection1.nick = "Codice-Drakebot";
 
     // Print out default connection details
 
-    writeln("Connecting too ", freenode.address, " at port ", freenode.port);
+    writeln("Connecting too ", Connection1.address, " at port 6667");
 
+    auto address = getAddress(Connection1.address, 6667);
 }
